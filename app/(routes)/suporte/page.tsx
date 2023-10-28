3125746035b4917f4a4e2687aac0b852feaf78ec
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-10-27 23:12:22 -0300

    create support page

---
+++ app/(routes)/suporte/page.tsx
@@ -0,0 +1,10 @@
+export default function SupportPage() {
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-screen">
+            <h1 className="text-3xl font-bold mb-4">Suporte</h1>
+            <p className="text-center text-gray-500">
+                Entre em contato com o suporte através do e-mail <a href="mailto:projetoclassificadobr@gmail.com" className="text-blue-500 hover:underline">projetoclassificadobr@gmail.com</a>
+            </p>
+        </div>
+    )
+}
