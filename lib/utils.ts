6b1b384d102bd8bbca5013bfe46572416864820f
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-10-27 23:14:49 -0300

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,34 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function getNumbersFromString(value: string): number {
+  return parseInt(value.replace(/\D/g, ""))
+}
+
+export function getAdModelName(ad: CarAd): string {
+  let modelName = ""
+
+  if (ad.model !== undefined && ad.model !== null) {
+    modelName = ad.model!.name
+  } else if (ad.customModel !== undefined) {
+    modelName = ad.customModel!
+  }
+
+  return modelName
+}
+
+export function getAdBrandName(ad: CarAd): string {
+  let brandName = ""
+
+  if (ad.brand !== undefined && ad.brand !== null) {
+    brandName = ad.brand!.name
+  } else if (ad.customBrand !== undefined) {
+    brandName = ad.customBrand!
+  }
+
+  return brandName
+}
