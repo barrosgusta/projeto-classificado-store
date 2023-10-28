8840d9adac2e29efb9bbbe015d706a6e423a9d50
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-10-27 23:12:15 -0300

    create main page

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import { getAds } from "@/actions/get-ads";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import AdList from "@/components/ui/ad-list";
+import ProductList from "@/components/ui/ad-list";
+import Container from "@/components/ui/container";
+
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic'
+
+export default async function HomePage() {
+    const ads = await getAds();
+    
+    return (
+        <Container>
+            <div className="space-y-10 pb-10 pt-5">
+                {/* <Billboard data={billboard}/> */}
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 h-full">
+                    <AdList title="Anúncios em destaque" items={ads}/>
+                </div>
+            </div>
+        </Container>
+    )
+}
